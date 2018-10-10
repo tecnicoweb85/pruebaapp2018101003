90841f0a562aa2c8e9c1d0486fc6f792b7e20d86
tecnicoweb85 <36242918+tecnicoweb85@users.noreply.github.com>
2018-10-10 18:00:43 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'app-android',
+		wp_ws_url : 'https://ivan.porelbierzo.es/wp-appkit-api/app-android',
+		wp_url : 'https://ivan.porelbierzo.es',
+		theme : 'q-android',
+		version : '1',
+		app_type : 'phonegap-build',
+		app_title : 'Iván López Freelance',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'p2X%gB.yMM,(,;zvZoNWaWtgWIqc)>(XZ:T?7Z$>7o=){W5k5v(&z5~~HHEMgAFn',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
